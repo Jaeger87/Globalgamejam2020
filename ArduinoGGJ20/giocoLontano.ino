a7feef8dbd7c26f4ef6033f4a36d8d4929e5db54
Andrea <rosati.1595834@gmail.com>
2020-02-02 12:18:20 +0100

    gioco lontano

--- ArduinoGGJ20/giocoLontano.ino
+++ ArduinoGGJ20/giocoLontano.ino
@@ -4,8 +4,6 @@
 //D = 1
 
 
-
-
 boolean giocoLontano()
 {
   statiLontano gameStatus = START;
@@ -13,34 +11,66 @@ boolean giocoLontano()
 
   while (!isOver)
   {
+    statiBOTTONE check = ERRORE;
     switch (gameStatus)
     {
       case START:
         setColorStrip(BLACK);
-        statiBOTTONE check = checkTelecomando(teleBPin, 4, teleDPin);
+        check = checkTelecomando(teleBPin, 4, teleDPin);
         if (check == OK)
           gameStatus = DEBUG;
-          
+
         break;
       case DEBUG:
         setColorStrip(RED);
+        check = checkTelecomando(redButtonPinRP, 5, blackButtonPinRP);
+        if (check == OK)
+          gameStatus = PREMIB;
+        else
+          gameStatus = START;
         break;
       case PREMIB:
         setColorStrip(CYAN);
+        check = checkTelecomando(teleBPin, 4, teleBPin);
+        if (check == OK)
+          gameStatus = NERO;
+        else
+          gameStatus = START;
         break;
+
       case NERO:
         setColorStrip(MAGENTA);
+        check = checkTelecomando(redButtonPinRP, 5, blackButtonPinRP);
+        if (check == OK)
+          gameStatus = GIALLO;
+        else
+          gameStatus = START;
         break;
       case GIALLO:
-
+        delayMio(1800);
+        check = checkTelecomando(redButtonPinRP, 5, yellowButtonPinRP);
+        if (check == OK)
+          gameStatus = STAMPA;
+        else
+          gameStatus = START;
         break;
       case STAMPA:
+        printer.println(F("Di all'operatore verde di premere i suoi tasti verdi\n\n"));
+        delayMio(500);
+        gameStatus = VERDEPREMEIVERDI;
         break;
       case VERDEPREMEIVERDI:
-
-        isOver = true;
+        int buttonRedOperatorState = digitalRead(greenButtonPinGreenP);
+        int buttonGreenOperatorState = digitalRead(buttonGreenOperator);
+        if (buttonRedOperatorState == HIGH && buttonGreenOperatorState == HIGH)
+          isOver = true;
         break;
     }
+
+    delayMio(50);
+
+    if (gameOver)
+      return false;
   }
   setColorStrip(BLACK);
   return true;
